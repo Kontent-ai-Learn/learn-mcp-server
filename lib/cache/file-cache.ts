d87407fd75ad8f901f255bc4d6e691faae617bc4
Richard Sustek <ennspassion@gmail.com>
2026-07-21 15:30:56 +0200

    implement file cache

--- lib/cache/file-cache.ts
+++ lib/cache/file-cache.ts
@@ -1,13 +1,25 @@
+import { existsSync, readFileSync, writeFileSync } from "node:fs";
 import type { ZodMiniType } from "zod/mini";
 
-export function getOrSetFromFileCache<T>({
-	key,
+export async function getOrSetFromFileCache<T>({
+	filePath,
 	value,
 	schema,
 }: {
-	readonly key: string;
+	readonly filePath: string;
 	readonly value: () => Promise<T>;
 	readonly schema: ZodMiniType<T>;
 }): Promise<T> {
-	throw new Error("Not implemented");
+	const resolveAndStoreValue = async () => {
+		const resolvedValue = await value();
+
+		writeFileSync(filePath, JSON.stringify(resolvedValue));
+		return resolvedValue;
+	};
+
+	if (!existsSync(filePath)) {
+		return await resolveAndStoreValue();
+	}
+	const existingContent = readFileSync(filePath, "utf8");
+	return schema.parse(JSON.parse(existingContent));
 }
