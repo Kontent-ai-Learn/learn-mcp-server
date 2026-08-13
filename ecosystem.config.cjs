52e38059c7b324d3018c88da4bb2740421c11e8e
Richard Sustek <ennspassion@gmail.com>
2026-08-13 10:39:14 +0200

    Fixes script

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
 	apps: [
 		{
 			name: "learn-mcp-server",
-			script: "node ./dist/bin.js shttp",
+			script: "./dist/bin.js shttp",
 			args: `start --port ${process.env.PORT || 8080} --keepAliveTimeout 70000`,
 			node_args: "",
 			watch: false,
